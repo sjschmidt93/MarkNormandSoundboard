ea0a942779e7257699a3f762323fd79d016369ba
sjschmidt93 <sjschmidt93@gmail.com>
2019-12-29 12:14:49 -0600

    added social media

--- CharacterSelect.tsx
+++ CharacterSelect.tsx
@@ -1,28 +1,47 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, Linking } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { NavigationScreenProp, NavigationState, NavigationParams } from 'react-navigation'
+import { SocialIcon } from 'react-native-elements'
 
 export interface NaivgationScreenProps {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>
 }
 
 export default class CharacterSelect extends React.Component<NaivgationScreenProps> {
-  onPressMark = () => this.props.navigation.navigate('SoundGrid', { comedian: 'Mark'})
+  onPressMark = () => {
+    this.props.navigation.navigate('SoundGrid', { comedian: 'Mark'})
+  }
 
-  onPressJoe = () => this.props.navigation.navigate('SoundGrid', { comedian: 'Joe' })
+  onPressJoe = () => {
+    this.props.navigation.navigate('SoundGrid', { comedian: 'Joe' })
+  }
 
   render() {
       return (
-        <View>
-          <Text>Choose your Comedian</Text>
+        <View style={styles.container}>
+          <Text style={styles.chooseText}>Choose your Comedian</Text>
           <View>
-            <TouchableOpacity onPress={this.onPressMark} style={styles.comedianButton}>
-
-            </TouchableOpacity>
-            <TouchableOpacity>
-
-            </TouchableOpacity>
+            <View style={styles.comedianButtonsRowContainer}>
+              <View style={styles.comedianButtonContainer}>
+                <TouchableOpacity onPress={this.onPressMark}>
+                  <Image source={require('./assets/images/mark_select_screen.jpg')} style={styles.comedianButton} />
+                </TouchableOpacity>
+                <Text style={styles.comedianText}>Mark Normand</Text>
+              </View>
+              <View style={styles.comedianButtonContainer}>
+                <TouchableOpacity onPress={this.onPressJoe}>
+                  <Image source={require('./assets/images/joe_select_screen.jpg')} style={styles.comedianButton} />
+                </TouchableOpacity>
+                <Text style={styles.comedianText}>Joe List</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.socialContainer}>
+            <SocialIcon type="twitter"    onPress={() => Linking.openURL('https://twitter.com/tuesdaystories?lang=en')} />
+            <SocialIcon type="facebook"   onPress={() => Linking.openURL('https://www.facebook.com/TuesdayStories/')} />
+            <SocialIcon type="soundcloud" onPress={() => Linking.openURL('https://soundcloud.com/tuesdays_with_stories')} />
+            <SocialIcon type="youtube"    onPress={() => Linking.openURL('https://www.youtube.com/channel/UCsE74YJvPJpaquzTPMO8hAA')} />
           </View>
         </View>
       )
@@ -30,9 +49,47 @@ export default class CharacterSelect extends React.Component<NaivgationScreenPro
 }
 
 const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    backgroundColor: '#032D46',
+    width: '100%',
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  chooseText: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  text: {
+    paddingTop: 5
+  },
+  comedianText: {
+    color: 'white',
+    paddingTop: 5
+  },
+  comedianButtonContainer: {
+    alignItems: 'center',
+    paddingHorizontal: 5
+  },
+  comedianButtonsRowContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingTop: 20
+  },
   comedianButton: {
-    backgroundColor: 'black',
-    height: 100,
-    width: 100
+    height: 400,
+    width: 175,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 4
+  },
+  socialContainer: {
+    alignItems: 'center',
+    paddingBottom: 20,
+    flexDirection: 'row',
+    paddingHorizontal: 30,
+    justifyContent: 'space-between'
   }
 })
